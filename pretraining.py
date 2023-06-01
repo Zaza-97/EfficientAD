014701bd929890fd297ba8ddc2476b9a5a3429a6
Zaza-97 <72982517+Zaza-97@users.noreply.github.com>
2023-06-01 08:45:32 +0200

    Update pretraining.py

--- pretraining.py
+++ pretraining.py
@@ -28,7 +28,7 @@ def get_argparse():
 # variables
 model_size = 'small'
 imagenet_train_path = '/kaggle/working/ImgNet_train/content/train' 
-state_path = ''
+state_path = '/kaggle/input/teacher-state-efficient-ad'
 PATH = 'model.pt' # aggiunto per ripredere training da checkpoint
 seed = 42
 on_gpu = torch.cuda.is_available()
